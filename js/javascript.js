302a6d8f5b876245a6646b2eda5caec3675a5401
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2022-12-01 15:17:02 +0000

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -2,16 +2,18 @@ window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
 
-    if (document.body.scrollTop > screen.height - 150 || document.documentElement.scrollTop > screen.height - 150) {
+    if (document.body.scrollTop > screen.height/2 || document.documentElement.scrollTop > screen.height/2) {
       document.getElementById("header").style.backgroundColor = "black";
       document.getElementById("header").style.color = "white";
       document.getElementById("logo").innerHTML = '<img src="./images/JTreatLogo_white.png" id="title"></img>'
 
 
     } else {
-      document.getElementById("header").style.backgroundColor = "white";
-      document.getElementById("header").style.color = "black";
-      document.getElementById("logo").innerHTML = '<img src="./images/JTreatLogo.png" id="title"></img>';
+      document.getElementById("header").style.backgroundColor = "#ffffff00";
+      document.getElementById("header").style.color = "white";
+      document.getElementById("logo").innerHTML = '<img src="./images/JTreatLogo_white.png" id="title"></img>';
     }
 
 }
+
+
