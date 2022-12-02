60fe9a96809e094e48e86b150496abbeef0dca71
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2022-12-02 12:41:38 +0000

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,29 @@
 window.onscroll = function () { scrollFunction() };
+window.onresize = function () { resizeBanner() };
 
-function scrollFunction() {
+resizeBanner();
 
-    if (document.body.scrollTop > screen.height - 250 || document.documentElement.scrollTop > screen.height - 250) {
-      document.getElementById("header").style.backgroundColor = "black";
-      document.getElementById("header").style.color = "white";
-
-
-    } else {
-      document.getElementById("header").style.backgroundColor = "#ffffff00";
-      document.getElementById("header").style.color = "white";
-    }
-
+function resizeBanner() {
+  if (screen.height > screen.width) {
+    document.getElementById("bannerBackground").style.height = "100%";
+    document.getElementById("bannerBackground").style.width = "auto";
+  } else {
+    document.getElementById("bannerBackground").style.width = "100%";
+    document.getElementById("bannerBackground").style.height = "auto";
+  }
 }
 
 
+function scrollFunction() {
+
+  if (document.body.scrollTop > screen.height - 300 || document.documentElement.scrollTop > screen.height - 300) {
+    document.getElementById("header").style.backgroundColor = "black";
+    document.getElementById("header").style.color = "white";
+
+
+  } else {
+    document.getElementById("header").style.backgroundColor = "#ffffff00";
+    document.getElementById("header").style.color = "white";
+  }
+
+}
