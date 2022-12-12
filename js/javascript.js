dc5226a53282e3186738df03d9bd3ecfd78a6c05
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2022-12-12 16:39:31 +0000

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -16,14 +16,42 @@ function resizeBanner() {
 
 function scrollFunction() {
 
-  if (document.body.scrollTop > screen.height - 300 || document.documentElement.scrollTop > screen.height - 300) {
-    document.getElementById("header").style.backgroundColor = "black";
-    document.getElementById("header").style.color = "white";
-
-
+  if (screen.width > 750) {
+    if (document.body.scrollTop > screen.height - 300 || document.documentElement.scrollTop > screen.height - 300) {
+      document.getElementById("header").style.backgroundColor = "black";
+      document.getElementById("header").style.color = "white";
+    } else {
+      document.getElementById("header").style.backgroundColor = "#ffffff00";
+      document.getElementById("header").style.color = "white";
+    }
   } else {
-    document.getElementById("header").style.backgroundColor = "#ffffff00";
-    document.getElementById("header").style.color = "white";
+    if (document.body.scrollTop > screen.height - 100 || document.documentElement.scrollTop > screen.height - 100) {
+      if (!menuOpen) {
+        document.getElementById("menuIcon").innerHTML = '<img src="./images/menuIconBlack.png" onclick="toggleMenu()"></img>'
+      }
+    } else {
+      document.getElementById("menuIcon").innerHTML = '<img src="./images/menuIconWhite.png" onclick="toggleMenu()"></img>'
+    }
   }
+}
 
+let menuOpen = false;
+function toggleMenu() {
+  if (screen.width < 750) {
+    if (!menuOpen) {
+      document.getElementById("menuIcon").style.rotate = "90deg";
+      document.getElementById("header").style.top = "0";
+      document.getElementById("menuIcon").innerHTML = '<img src="./images/menuIconWhite.png" onclick="toggleMenu()"></img>';
+      menuOpen = true;
+    } else {
+      document.getElementById("menuIcon").style.rotate = "0deg";
+      document.getElementById("header").style.top = "-250px";
+      if (document.body.scrollTop > screen.height - 100 || document.documentElement.scrollTop > screen.height - 100) {
+        setTimeout(function () {
+          document.getElementById("menuIcon").innerHTML = '<img src="./images/menuIconBlack.png" onclick="toggleMenu()"></img>'
+        }, 500);
+      }
+      menuOpen = false;
+    }
+  }
 }
