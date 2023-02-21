3779f3a9b6d77c6199b036736d361b33e2a66057
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2023-02-21 20:25:27 +0000

    commit

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -7,12 +7,12 @@ function Booking() {
             <div className="booking-option">
                 <h3 className="uppercase-text">25 Minute Consultation</h3>
                 <p>£12.50</p>
-                <a href="https://calendly.com/jtreat_/25min?hide_gdpr_banner=1&primary_color=ff0004&back=1&month=2023-02" target="_blank"><button>Book</button></a>
+                <a href="https://calendly.com/jtreat_/25min?hide_gdpr_banner=1&primary_color=ff0004&back=1&month=2023-02" target="_blank" rel="noreferrer"><button>Book</button></a>
             </div>
             <div className="booking-option">
                 <h3 className="uppercase-text">50 Minute Consultation</h3>
                 <p>£19.50</p>
-                <a href="https://calendly.com/jtreat_/25min?hide_gdpr_banner=1&primary_color=ff0004&back=1&month=2023-02" target="_blank"><button>Book</button></a>
+                <a href="https://calendly.com/jtreat_/25min?hide_gdpr_banner=1&primary_color=ff0004&back=1&month=2023-02" target="_blank" rel="noreferrer"><button>Book</button></a>
             </div>
         </>
     )
